d4236984203a36a39e6e96afab7c99ef8a414fe1
Apply Hi Tech <dar2144@columbia.edu>
2016-07-26 17:54:38 -0400

    More of Tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+
 # Create your views here.
 from .models import Question
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ','.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+#	output = ','.join([q.question_text for q in latest_question_list])
+	return render(request,'polls/index.html',context)
 
-
-def first(request):
-	return HttpResponse("The first poll")
-
-def second(request):
-	return HttpResponse("The second url")
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at Question %s." %question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request,'polls/detail.html',{'question':question})
+
 
 def results(request, question_id):
     return HttpResponse("Response %s" % question_id) #Edited from Tutorial.
