3cd163caafa0d51203472238aea52abcd902db6c
Apply Hi Tech <dar2144@columbia.edu>
2016-07-26 15:36:04 -0400

    Middle of Tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Question
 
 def index(request):
-	return HttpResponse("hello World! You're at the polls index")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ','.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
 
 
 def first(request):
@@ -11,3 +14,12 @@ def first(request):
 
 def second(request):
 	return HttpResponse("The second url")
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at Question %s." %question_id)
+
+def results(request, question_id):
+    return HttpResponse("Response %s" % question_id) #Edited from Tutorial.
+ 
+def vote(request, question_id):
+	return HttpResponse("You're voting on  question %s." %question_id)
