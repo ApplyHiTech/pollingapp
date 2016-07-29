61eb9e773e44503964ecca17d1f014879121f06a
Apply Hi Tech <dar2144@columbia.edu>
2016-07-29 10:39:08 -0400

    tests added

--- polls/views.py
+++ polls/views.py
@@ -12,10 +12,12 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """return last 5 published questions"""
-
-
-        return Question.objects.fitler(pub_date_lte=timezone.now()).order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 
